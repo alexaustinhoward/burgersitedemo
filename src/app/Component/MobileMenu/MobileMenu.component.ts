6f2b4839cdfff4334c8cc43fead9aeb5878f2d08
alexander austin howard <blazerv4@gmail.com>
2020-06-07 22:31:47 -0400

    first component added

---
+++ src/app/Component/MobileMenu/MobileMenu.component.ts
@@ -0,0 +1,15 @@
+import {Component,OnInit} from'@angular/core'
+@Component({
+    selector: 'MobileMenu',
+    template:'./MobileMenu.component.html',
+    styleUrls:['./MobileMenu.component.scss']
+})
+export class MobileMenuComponent implements OnInit{
+    constructor()
+    {
+
+    }
+    ngOnInit(){
+        
+    }
+}
---
+++ src/app/Component/NavigationBar/NavigationBar.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core'
+@Component({
+    selector: 'NavigationBar',
+    template:'./NavigationBar.component.html',
+    styleUrls:['./NavigationBar.component.scss']
+})
+export class NavigationBarComponent implements OnInit{
+constructor(){}
+ngOnInit(){
+    
+}
+}
