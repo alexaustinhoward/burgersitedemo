2c980c30705d1d4190523a71560fbffa08669535
alexander austin howard <blazerv4@gmail.com>
2020-08-12 20:15:52 -0400

    added congnito mobilemenu and updated the css

--- src/app/Component/MobileMenu/MobileMenu.component.ts
+++ src/app/Component/MobileMenu/MobileMenu.component.ts
@@ -1,4 +1,6 @@
-import {Component,OnInit} from'@angular/core'
+import {Component,OnInit,ChangeDetectorRef } from'@angular/core';
+
+import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
 @Component({
     selector: 'MobileMenu',
     templateUrl:'./MobileMenu.component.html',
@@ -6,16 +8,34 @@ import {Component,OnInit} from'@angular/core'
 })
 export class MobileMenuComponent implements OnInit{
     toggleiconstatus:boolean=false;
-    constructor()
-    {
-    }
-    ngOnInit(){
-    }
-    OnToggleIcon(){
+    title = 'amplify-angular-auth';
+    user: CognitoUserInterface | undefined;
+    togglesignin:boolean=false;
+    authState: AuthState;
+constructor(private ref: ChangeDetectorRef){}
+ngOnInit(){
+    onAuthUIStateChange((authState, authData) => {
+        this.authState = authState;
+        this.user = authData as CognitoUserInterface;
+        this.ref.markForCheck()
+      })
+      
+}
+ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
+  OnToggleSignInIcon(){
+    this.togglesignin=!this.togglesignin;
+    console.log(this.togglesignin);
+}
+GetSigninStatus(){
+    return this.togglesignin === true ?'block':'none';
+}
+ OnToggleIcon(){
         this.toggleiconstatus=!this.toggleiconstatus;
         console.log(this.toggleiconstatus);
     }
-    GetStatus(){
+ GetStatus(){
         return this.toggleiconstatus === true ?'block':'none';
     }
 }
